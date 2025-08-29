51931b2f62d475c436b9e8292b8fc477f18971fa
Sivsai <sivasai299792@gmail.com>
2025-08-29 17:36:34 +0530

    Final version

--- LittleLemonapp/app/src/main/java/com/examle/littlelemonapp/Profile.kt
+++ LittleLemonapp/app/src/main/java/com/examle/littlelemonapp/Profile.kt
@@ -1,24 +1,33 @@
 package com.examle.littlelemonapp
 
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 
+@SuppressLint("UseKtx")
 @Composable
 fun ProfileScreen(navController: NavController, context: Context) {
     val sharedPref = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
